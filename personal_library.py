33bc64c3720b21f82181b9407139bfe842cdeb66
Gaviola <69123521+Gaviola@users.noreply.github.com>
2021-06-12 17:52:44 -0300

    Update personal_library.py

--- personal_library.py
+++ personal_library.py
@@ -8,26 +8,26 @@ def create(Carpeta):
   T = trie.Trie()
   llenarTrie(T,Carpeta)
  
- #Coloca todas las palabras de los documentos dentro del trie
- def llenarTrie(T,Carpeta):
+#Coloca todas las palabras de los documentos dentro del trie
+def llenarTrie(T,Carpeta):
   Docs = os.listdir(Carpeta)
     #Recorremos todos los documentos
     for x in range(len(Docs)):
       with open(carpeta + "/" + Docs[x],"r") as Doc:
-            Lines = doc.readlines()
-            #Recorremos todas las lineas dentro del documento
-            for y in range(len(Lines)):
-              Line = algo.String(Lines[y])
-              start = 0
-              #Recorremos todas las palabras dentro de la linea y las insertamos en el trie
-              for z in range(len(Line)):
-                if Line[z] == " ":
-                  Word = algo.substr(Line,start,z)
-                  start =  z 
-                  trie.insert(T,Word,Doc)
-              else:
-                Word = algo.substr(Line,start,z)
-                tire.insert(T,Word,Doc)
+        Lines = Doc.readlines()
+        #Recorremos todas las lineas dentro del documento
+        for y in range(len(Lines)):
+          Line = algo.String(Lines[y])
+          start = 0
+          #Recorremos todas las palabras dentro de la linea y las insertamos en el trie
+          for z in range(len(Line)):
+            if Line[z] == " ":
+              Word = algo.substr(Line,start,z)
+              start =  z 
+              trie.insert(T,Word,Doc)
+            else:
+              Word = algo.substr(Line,start,z)
+              trie.insert(T,Word,Doc)
         
   
 
